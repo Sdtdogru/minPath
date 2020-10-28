e828bd3e097cabddb02cfffa7fb99b606a0676ac
Sedat Doğru <sedat.dogru@etiya.com>
2020-10-28 09:23:22 +0300

    02

--- minimumTrianglePathsProblem/src/com/company/Main.java
+++ minimumTrianglePathsProblem/src/com/company/Main.java
@@ -28,18 +28,3 @@ public class Main {
         }
     }
 }
-/*
-* soruda verilen ağaç yapısının min değerini  yolun bulmak için yapımızın sonundan başlayarak aşağıdan yukarıya doğru bir tolama
-* ve küçük olan değeri seçme işlemi yaparak gerçekleşicektir.
-*
-* elimde bulunan iki boyutlu bir dizinin son dizisini ele alarak başladım bu son diziyi yeni olan ve toplam işlemlerini üzerinde
-* gerçekleştireceğim yeni bir dizi oluştudum ve iki boyutlu dizinin son dizisini buraya taşıdım.
-*
-* artık elimde bulunan yeni dizinin elemanarını her seferinde iki boyutlu dizinin bir önceki indexsine geçicek şekilde bir dögünü içinde
-* şu şekilde bir işlem yapmaktayım eski ereyimin i'ninci satırındaki j'ninci suntununda bulunan sayı ile yeni dizimdeki j'nin ve j+1. sizindeki küçük olan
-* elemanla topladım ve elimde yini bir sayı oluşmakta bu sayı yukarı doğru oluşan toplama işleminde en küçük sayıyı vermektedir
-*
-*
-*
-*
-* */
